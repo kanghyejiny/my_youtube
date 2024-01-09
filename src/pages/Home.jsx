5e5705db56e40d5454826c2f65abf6185194c3c5
kanghyejiny <gpwls2779@gmail.com>
2024-01-09 16:01:03 +0900

    메인 비디오 공통 요소 컴포넌트 만들기, Swiper 슬라이드 작업, 로딩 효과 구현 등 비디오 컴포넌트 통합 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Reactb from '../components/contents/Reactb'
-import Jsb from '../components/contents/Jsb'
-import Vue from '../components/contents/Vue'
-import Webserver from '../components/contents/Webserver'
+import VideoSlider from '../components/videos/VideoSlider'
+
+import { reactbText } from '../data/reactb'
+import { jsbText } from '../data/jsb'
+import { vueText } from '../data/vue'
+import { webserverText } from '../data/webserver'
 
 const Home = () => {
   return (
     <Main title='Youtube' description='코딩애플 유튜버 사이트에 오신 것을 환영합니다.'>
       <Today />
       <Developer />
-      <Reactb />
-      <Jsb />
-      <Vue />
-      <Webserver />
+      <VideoSlider videos={reactbText} title='리액트 기초 : 블로그 프로젝트 만들기' id='reactb' />
+      <VideoSlider videos={jsbText} title='웹개발로 배우는 자바스크립트 기초' id='jsb' />
+      <VideoSlider videos={vueText} title='Vue 로 만드는 직방 / 인스타그램 웹앱' id='vue' />
+      <VideoSlider videos={webserverText} title='2시간만에 웹서버 쉽게 개발하기 (Node.js + MongoDB)' id='webserver' />
     </Main>
   )
 }
